31f9a7a0738911a0c86c7b45360cb022a8d6ec4d
admin <admin@example.com>
2021-12-08 20:19:12 +0800

    重复的switch_refactorStep_v0.1——回退代码

--- 12.repeated-switches/repeated-switches-init/src/main/java/com/huawei/repeated_switches_init/server/KeepPetCosts.java
+++ 12.repeated-switches/repeated-switches-init/src/main/java/com/huawei/repeated_switches_init/server/KeepPetCosts.java
@@ -6,8 +6,7 @@ package com.huawei.repeated_switches_init.server;
 
 import com.huawei.repeated_switches_init.model.Costs;
 import com.huawei.repeated_switches_init.model.Pet;
-import com.huawei.repeated_switches_init.server.cost.pet.PetCosts;
-import com.huawei.repeated_switches_init.server.cost.PetCostsFactory;
+import com.huawei.repeated_switches_init.server.cost.PetCosts;
 
 /**
  * 饲养宠物耗费的精力
@@ -22,7 +21,7 @@ public class KeepPetCosts {
      * @return 耗费的精力
      */
     public Costs getKeepPetCosts(Pet pet) {
-        PetCosts petCosts = new PetCostsFactory().createPetCosts(pet);
+        PetCosts petCosts = new PetCosts(pet);
         return new Costs(pet.getSpecies(), petCosts.getDailyPay(),
             petCosts.getDailyCompanyTime(), petCosts.getCleanFrequency());
     }
---
+++ 12.repeated-switches/repeated-switches-init/src/main/java/com/huawei/repeated_switches_init/server/cost/PetCosts.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+ */
+
+package com.huawei.repeated_switches_init.server.cost;
+
+import com.huawei.repeated_switches_init.model.Pet;
+
+/**
+ * 饲养宠物耗费精力
+ *
+ * @since 2021-06-18
+ */
+public class PetCosts {
+    private final Pet pet;
+
+    public PetCosts(Pet pet) {
+        this.pet = pet;
+    }
+
+    /**
+     * 每天需要陪伴的时间
+     * 
+     * @return 每天需要陪伴的时间
+     */
+    public String getDailyCompanyTime() {
+        switch (pet.getSpecies()) {
+            case "Dog":
+                return 90 + 60 * pet.getQuantity() + " minutes";
+            case "Cat":
+                return 60 * pet.getQuantity() + " minutes";
+            case "Fish":
+                return "20 minutes";
+            default:
+                return "unknown";
+        }
+    }
+
+    /**
+     * 每天需要花费的金钱
+     * 
+     * @return 每天需要花费的金钱
+     */
+    public double getDailyPay() {
+        switch (pet.getSpecies()) {
+            case "Dog":
+                return pet.getDailyFeedingPay() * pet.getQuantity() * 1.5;
+            case "Cat":
+                return pet.getDailyFeedingPay() * pet.getQuantity() * 1.2;
+            case "Fish":
+                return pet.getDailyFeedingPay() * pet.getQuantity() + 2;
+            default:
+                return -1;
+        }
+    }
+
+    /**
+     * 清理频率
+     * 
+     * @return 清理频率
+     */
+    public String getCleanFrequency() {
+        switch (pet.getSpecies()) {
+            case "Dog":
+                return "wash every 2 weeks";
+            case "Cat":
+                return "wash every 6 weeks";
+            case "Fish":
+                return "change the water every 10 days";
+            default:
+                return "unknown";
+        }
+    }
+}
