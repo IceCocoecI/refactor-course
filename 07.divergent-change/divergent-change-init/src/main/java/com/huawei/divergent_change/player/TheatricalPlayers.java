24dcea041bb0481f3cd131dbcdc19dba99d36927
admin <admin@example.com>
2021-12-08 11:39:16 +0800

    发散式变化_refactorStep_v0.1——17 根据IDEA提示，将字段变为final ->光标放在 TheatricalPlayers 类的 performanceRepository 字段上（上一步提炼的字段） ->Alt+Enter（IDEA会以底色提示） ->选择 Make 'performanceRepository' 'final' ->确认
    注：
    1.如果没有提示可变为final，可以在settings->Editor->Inspections->Code style issues->勾选field may be final
    2.根据不同版本和不同配置，可能有些同学提炼出来直接就是final字段，就无需此步操作

--- 07.divergent-change/divergent-change-init/src/main/java/com/huawei/divergent_change/player/TheatricalPlayers.java
+++ 07.divergent-change/divergent-change-init/src/main/java/com/huawei/divergent_change/player/TheatricalPlayers.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class TheatricalPlayers {
 
-    private PerformanceRepositoryImpl performanceRepository;
+    private final PerformanceRepositoryImpl performanceRepository;
 
     public TheatricalPlayers() {
         performanceRepository = new PerformanceRepositoryImpl();
