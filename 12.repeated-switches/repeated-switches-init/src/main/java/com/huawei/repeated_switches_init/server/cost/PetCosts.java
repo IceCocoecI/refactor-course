815b80b9e066ae76ee286363ee4c2faeb7092496
admin <admin@example.com>
2021-12-08 19:59:47 +0800

    重复的switch_refactorStep_v0.1——8 将父类方法下推子类中：下推 ->重复前面下推步骤，光标放在类的空白位置 ->Ctrl+Alt+Shift+T ->选择：Push Members Down ->勾选 getDailyCompanyTime、getDailyPay、getCleanFrequency 3个方法 ->分别勾选3个方法右侧的：Keep abstract ->确认 ->提示我们某些特定场景中下推的成员可能无法被访问 ->点击 Continue 忽略，让IDEA及测试帮我们识别是否真的有问题 ->执行测试可通过，说明无调用问题
    注：
    这里勾选 keep abstract，因为父类中不需要默认方法，全部由子类实现，但需要保留公共接口

--- 12.repeated-switches/repeated-switches-init/src/main/java/com/huawei/repeated_switches_init/server/cost/PetCosts.java
+++ 12.repeated-switches/repeated-switches-init/src/main/java/com/huawei/repeated_switches_init/server/cost/PetCosts.java
@@ -15,7 +15,7 @@ import com.huawei.repeated_switches_init.server.cost.pet.UnknownPet;
  *
  * @since 2021-06-18
  */
-public class PetCosts {
+public abstract class PetCosts {
     protected final Pet pet;
 
     protected PetCosts(Pet pet) {
@@ -27,52 +27,19 @@ public class PetCosts {
      * 
      * @return 每天需要陪伴的时间
      */
-    public String getDailyCompanyTime() {
-        switch (pet.getSpecies()) {
-            case "Dog":
-                return 90 + 60 * pet.getQuantity() + " minutes";
-            case "Cat":
-                return 60 * pet.getQuantity() + " minutes";
-            case "Fish":
-                return "20 minutes";
-            default:
-                return "unknown";
-        }
-    }
+    public abstract String getDailyCompanyTime();
 
     /**
      * 每天需要花费的金钱
      * 
      * @return 每天需要花费的金钱
      */
-    public double getDailyPay() {
-        switch (pet.getSpecies()) {
-            case "Dog":
-                return pet.getDailyFeedingPay() * pet.getQuantity() * 1.5;
-            case "Cat":
-                return pet.getDailyFeedingPay() * pet.getQuantity() * 1.2;
-            case "Fish":
-                return pet.getDailyFeedingPay() * pet.getQuantity() + 2;
-            default:
-                return -1;
-        }
-    }
+    public abstract double getDailyPay();
 
     /**
      * 清理频率
      * 
      * @return 清理频率
      */
-    public String getCleanFrequency() {
-        switch (pet.getSpecies()) {
-            case "Dog":
-                return "wash every 2 weeks";
-            case "Cat":
-                return "wash every 6 weeks";
-            case "Fish":
-                return "change the water every 10 days";
-            default:
-                return "unknown";
-        }
-    }
+    public abstract String getCleanFrequency();
 }
--- 12.repeated-switches/repeated-switches-init/src/main/java/com/huawei/repeated_switches_init/server/cost/pet/UnknownPet.java
+++ 12.repeated-switches/repeated-switches-init/src/main/java/com/huawei/repeated_switches_init/server/cost/pet/UnknownPet.java
@@ -12,4 +12,46 @@ public class UnknownPet extends PetCosts {
     public UnknownPet(Pet pet) {
         super(pet);
     }
+
+    @Override
+    public String getDailyCompanyTime() {
+        switch (pet.getSpecies()) {
+            case "Dog":
+                return 90 + 60 * pet.getQuantity() + " minutes";
+            case "Cat":
+                return 60 * pet.getQuantity() + " minutes";
+            case "Fish":
+                return "20 minutes";
+            default:
+                return "unknown";
+        }
+    }
+
+    @Override
+    public double getDailyPay() {
+        switch (pet.getSpecies()) {
+            case "Dog":
+                return pet.getDailyFeedingPay() * pet.getQuantity() * 1.5;
+            case "Cat":
+                return pet.getDailyFeedingPay() * pet.getQuantity() * 1.2;
+            case "Fish":
+                return pet.getDailyFeedingPay() * pet.getQuantity() + 2;
+            default:
+                return -1;
+        }
+    }
+
+    @Override
+    public String getCleanFrequency() {
+        switch (pet.getSpecies()) {
+            case "Dog":
+                return "wash every 2 weeks";
+            case "Cat":
+                return "wash every 6 weeks";
+            case "Fish":
+                return "change the water every 10 days";
+            default:
+                return "unknown";
+        }
+    }
 }
