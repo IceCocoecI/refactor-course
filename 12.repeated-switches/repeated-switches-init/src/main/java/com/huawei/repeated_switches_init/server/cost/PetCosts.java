b5a9b2c2da1f71768feb1e320183f5eaf12f1fdd
admin <admin@example.com>
2021-12-08 17:44:33 +0800

    重复的switch_refactorStep_v0.1——3 使用多态，使工厂返回PetCosts的各个子类对象：替换父类对象 ->进入 PetCosts 类的 createPetCosts 方法 ->注释掉：return new PetCosts(pet); ->补充创建子类实例的 switch 逻辑 ->运行测试确保通过
    注：
    1.直接返回子类，是多态取代重复switch的一个关键点，如果我们无法改变对客户端暴露的接口，就将多个switch合并成一个，用工厂等模式创建子类实例对象
    2.当前IDEA无法直接重构为多态，我们需要先注释掉return new PetCosts，然后手动添加switch。手动操作有一定的危险性，所以一定要有测试覆盖

--- 12.repeated-switches/repeated-switches-init/src/main/java/com/huawei/repeated_switches_init/server/cost/PetCosts.java
+++ 12.repeated-switches/repeated-switches-init/src/main/java/com/huawei/repeated_switches_init/server/cost/PetCosts.java
@@ -5,6 +5,10 @@
 package com.huawei.repeated_switches_init.server.cost;
 
 import com.huawei.repeated_switches_init.model.Pet;
+import com.huawei.repeated_switches_init.server.cost.pet.CatCosts;
+import com.huawei.repeated_switches_init.server.cost.pet.DogCosts;
+import com.huawei.repeated_switches_init.server.cost.pet.FishCosts;
+import com.huawei.repeated_switches_init.server.cost.pet.UnknownPet;
 
 /**
  * 饲养宠物耗费精力
@@ -19,7 +23,17 @@ public class PetCosts {
     }
 
     public static PetCosts createPetCosts(Pet pet) {
-        return new PetCosts(pet);
+        switch (pet.getSpecies()) {
+            case "Dog":
+                return new DogCosts(pet);
+            case "Cat":
+                return new CatCosts(pet);
+            case "Fish":
+                return new FishCosts(pet);
+            default:
+                return new UnknownPet(pet);
+        }
+//        return new PetCosts(pet);
     }
 
     /**
