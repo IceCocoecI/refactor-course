916bedfaaf81a558e8ae445ba48643a6658a246e
admin <admin@example.com>
2021-12-09 16:15:40 +0800

    冗赘的元素_refactorStep_v0.1——17 内联 HouseData 方式二：处理调用位置，直接调用 HouseData ->找到 House 类的调用位置（HouseTest） ->直接调用 houseData，而不是通过 house 获取 ->执行用例确保通过 ->Alt+Delete 安全删除冗余变量 buyerSalary 和 house ->Shift+F6 重命名 instance 为 calendar

--- 14.lazy-element/lazy-element-init/src/test/java/com/huawei/lazy_element_init/houseinfo/HouseTest.java
+++ 14.lazy-element/lazy-element-init/src/test/java/com/huawei/lazy_element_init/houseinfo/HouseTest.java
@@ -18,17 +18,16 @@ public class HouseTest {
     @Test
     public void should_get_house_info() {
         final double tax = 0.2;
-        final int buyerSalary = 15000;
-        final Calendar instance = Calendar.getInstance();
-        instance.set(2015, Calendar.JULY, 8);
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(2015, Calendar.JULY, 8);
 
-        final House house = new House(new HouseData(150, 80000, instance));
+        final HouseData houseData = new HouseData(150, 80000, calendar);
 
-        assertTrue(Math.abs(house.getHouseData().getSquare() - 150.0) < 1e-4);
-        assertTrue(house.houseData.isBigHouse());
-        assertEquals(6, house.houseData.calculateHouseAge());
-        assertTrue(Math.abs(house.getHouseData().getUnitPrice() - 80000.0) < 1e-4);
-        assertTrue(Math.abs(house.houseData.getTotalPrice(tax) - 1.44E7) < 1e-4);
-        assertTrue(Math.abs(house.houseData.getDownPayment(true, tax) - 5040000.0) < 1e-4);
+        assertTrue(Math.abs(houseData.getSquare() - 150.0) < 1e-4);
+        assertTrue(houseData.isBigHouse());
+        assertEquals(6, houseData.calculateHouseAge());
+        assertTrue(Math.abs(houseData.getUnitPrice() - 80000.0) < 1e-4);
+        assertTrue(Math.abs(houseData.getTotalPrice(tax) - 1.44E7) < 1e-4);
+        assertTrue(Math.abs(houseData.getDownPayment(true, tax) - 5040000.0) < 1e-4);
     }
 }
