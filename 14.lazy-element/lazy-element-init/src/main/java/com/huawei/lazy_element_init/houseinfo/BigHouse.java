9620ed22577a32754dd19fb0f0aa8e0a536e702f
admin <admin@example.com>
2021-12-09 15:00:15 +0800

    冗赘的元素_refactorStep_v0.1——4 通过内联超类，折叠继承体系，消除不必要的继承关系 ->进入 BigHouse 类 ->光标放在 extends House 中的父类名 House 上 ->Ctrl+Alt+N 或 在重构面板选择：Inline Super Class ->选择 Inline all references and remove the class ->确认

--- 14.lazy-element/lazy-element-init/src/main/java/com/huawei/lazy_element_init/houseinfo/BigHouse.java
+++ 14.lazy-element/lazy-element-init/src/main/java/com/huawei/lazy_element_init/houseinfo/BigHouse.java
@@ -4,6 +4,8 @@
 
 package com.huawei.lazy_element_init.houseinfo;
 
+import java.util.Calendar;
+
 import com.huawei.lazy_element_init.houseinfo.housedata.HouseData;
 
 /**
@@ -11,11 +13,17 @@ import com.huawei.lazy_element_init.houseinfo.housedata.HouseData;
  *
  * @since 2021-11-08
  */
-public class BigHouse extends House {
+public class BigHouse {
     private static final int BIG_HOUSE_AREA = 140;
+    private static final double FIRST_HOUSE_PAY_RATE = 0.35;
+    private static final double NOT_FIRST_HOUSE_PAY_RATE = 0.7;
+    /**
+     * 房屋信息数据
+     */
+    protected final HouseData houseData;
 
     public BigHouse(HouseData houseData) {
-        super(houseData);
+        this.houseData = houseData;
     }
 
     /**
@@ -25,5 +33,54 @@ public class BigHouse extends House {
      */
     public boolean isBigHouse() {
         return houseData.getSquare() > BIG_HOUSE_AREA;
+    }
+
+    /**
+     * 计算房屋总价
+     *
+     * @param tax 税率
+     * @return 总价
+     */
+    public double getTotalPrice(double tax) {
+        return houseData.getSquare() * houseData.getUnitPrice() * (1 + tax);
+    }
+
+    /**
+     * 计算购房首付款
+     *
+     * @param isFirstHouse 是否首套房
+     * @param tax 税率
+     * @return 购房首付款
+     */
+    public double getDownPayment(boolean isFirstHouse, double tax) {
+        return isFirstHouse ? FIRST_HOUSE_PAY_RATE * getTotalPrice(tax) : NOT_FIRST_HOUSE_PAY_RATE * getTotalPrice(tax);
+    }
+
+    /**
+     * 计算房龄
+     *
+     * @return 房龄
+     */
+    public int calculateHouseAge() {
+        final int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        return currentYear - houseData.getCompletionDate().get(Calendar.YEAR);
+    }
+
+    /**
+     * 获取房屋面积
+     *
+     * @return 房屋面积
+     */
+    public double getSquare() {
+        return houseData.getSquare();
+    }
+
+    /**
+     * 获取房屋单价
+     *
+     * @return 单价
+     */
+    public double getUnitPrice() {
+        return houseData.getUnitPrice();
     }
 }
