28e3946da8fddb2b6d6ed6ddcb0284e5c269708b
admin <admin@example.com>
2021-12-09 15:42:33 +0800

    冗赘的元素_refactorStep_v0.1——10 内联类，消除HouseData贫血对象：更改调用处，消除对 HouseData 的引用 ->查找 House 类的调用位置（HouseTest） ->直接将 houseData 的构造参数传入  new House 构造参数 ->运行测试确保通过 ->光标分别放在 houseData 和 buyerSalary 冗余变量上 ->分别 Alt+Delete 或 在重构面板/Alt+Enter 选择：Safe Delete ->分别 确认 ->Shift+F6 重命名 instance 为 calendar ->Ctrl+Alt+O 或 直接删除 消除无用import

--- 14.lazy-element/lazy-element-init/src/test/java/com/huawei/lazy_element_init/houseinfo/HouseTest.java
+++ 14.lazy-element/lazy-element-init/src/test/java/com/huawei/lazy_element_init/houseinfo/HouseTest.java
@@ -7,8 +7,6 @@ import java.util.Calendar;
 
 import org.junit.jupiter.api.Test;
 
-import com.huawei.lazy_element_init.houseinfo.housedata.HouseData;
-
 /**
  * House测试类
  *
@@ -18,12 +16,10 @@ public class HouseTest {
     @Test
     public void should_get_house_info() {
         final double tax = 0.2;
-        final int buyerSalary = 15000;
-        final Calendar instance = Calendar.getInstance();
-        instance.set(2015, Calendar.JULY, 8);
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(2015, Calendar.JULY, 8);
 
-        final HouseData houseData = new HouseData(150, 80000, instance);
-        final House house = new House(houseData.getSquare(), houseData.getUnitPrice(), houseData.getCompletionDate());
+        final House house = new House(150, 80000, calendar);
 
         assertTrue(Math.abs(house.getSquare() - 150.0) < 1e-4);
         assertTrue(house.isBigHouse());
