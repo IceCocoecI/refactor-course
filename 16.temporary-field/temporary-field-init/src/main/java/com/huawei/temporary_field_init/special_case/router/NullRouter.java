3e571170f3cda5119faefb4e1afa634c4e42abd8
admin <admin@example.com>
2022-03-03 17:22:33 +0800

    临时字段_refactorStep_v0.1——14 为 Router 接口添加 NullRouter 实现类 ->进入 Router 接口 ->光标放在接口名 Router 上 ->Alt+Enter ->选择 Implement interface ->Name 填写：NullRouter ->确认 ->弹出 Select Method to Implement 面板中，确认选择实现 route 方法（默认已选） ->OK 确认

---
+++ 16.temporary-field/temporary-field-init/src/main/java/com/huawei/temporary_field_init/special_case/router/NullRouter.java
@@ -0,0 +1,15 @@
+package com.huawei.temporary_field_init.special_case.router;
+
+import com.huawei.temporary_field_init.special_case.Message;
+
+/**
+ * 功能描述
+ *
+ * @since 2021-12-10
+ */
+public class NullRouter implements Router {
+    @Override
+    public String route(Message body) {
+        return null;
+    }
+}
