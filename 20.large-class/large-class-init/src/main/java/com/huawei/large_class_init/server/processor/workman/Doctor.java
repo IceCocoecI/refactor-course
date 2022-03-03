f18c39acbee57d4d497e8c70fbbd139848679494
admin <admin@example.com>
2022-03-03 17:29:34 +0800

    过大的类_refactorStep_v0.1——12 提取子类：同步骤11，在 workman 包下再分别创建 Workman 的 Doctor、InvalidWorkman 子类

---
+++ 20.large-class/large-class-init/src/main/java/com/huawei/large_class_init/server/processor/workman/Doctor.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+ */
+
+package com.huawei.large_class_init.server.processor.workman;
+
+import com.huawei.large_class_init.server.processor.Workman;
+import com.huawei.large_class_init.server.processor.enums.Gender;
+import com.huawei.large_class_init.server.processor.model.CareerInfo;
+import com.huawei.large_class_init.server.processor.model.Contacts;
+
+/**
+ * 功能描述
+ *
+ * @since 2021-12-13
+ */
+public class Doctor extends Workman {
+    public Doctor(String name, Gender gender, CareerInfo careerInfo, Contacts contacts) {
+        super(name, gender, careerInfo, contacts);
+    }
+}
