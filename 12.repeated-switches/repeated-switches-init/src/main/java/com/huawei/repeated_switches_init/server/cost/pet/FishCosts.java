507f8feae4c1261e9b1901a4712f1397304efba7
admin <admin@example.com>
2022-03-03 17:00:48 +0800

    重复的switch_refactorStep_v0.1——1 创建 PetCosts 的子类：DogCosts、CatCosts、FishCosts、UnknownPet ->找到 PetCosts 类 ->光标放在类名上 ->Alt+Enter ->Create subclass ->输入类名：DogCosts ->输入目标package：com.huawei.repeated_switches_init.server.cost.pet（此时pet包不存在，IDEA会自动帮我们创建） ->确认 ->回到 PetCosts 类 ->重复上面步骤，继续创建 CatCosts、FishCosts、UnknownPet 子类

---
+++ 12.repeated-switches/repeated-switches-init/src/main/java/com/huawei/repeated_switches_init/server/cost/pet/FishCosts.java
@@ -0,0 +1,15 @@
+package com.huawei.repeated_switches_init.server.cost.pet;
+
+import com.huawei.repeated_switches_init.model.Pet;
+import com.huawei.repeated_switches_init.server.cost.PetCosts;
+
+/**
+ * 功能描述
+ *
+ * @since 2021-12-08
+ */
+public class FishCosts extends PetCosts {
+    public FishCosts(Pet pet) {
+        super(pet);
+    }
+}
